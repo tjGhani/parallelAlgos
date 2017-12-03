048e23152693a9012d8d7003b4e8f028dd7e9eea
tjGhani <tahira.ghani18@gmail.com>
2017-12-03 17:21:52 -0500

    updated texture

--- cadProcessing.m
+++ cadProcessing.m
@@ -33,7 +33,8 @@ function arrSlice =  cadProcessing(workingDir, first, last)
         [detectedBH blackHole blackBoundary blackArea] = detectBlackHoles(lungWindow, originalOnWhite);
 		[detectedWM whiteMass whiteBoundary whiteArea] = whiteMassDetection(lungWindow, originalOnBlack);
         %textureFeatures = textureAnalysis(bwLungMed, lungWindow, depth);
-        textureFeatures = textureAnalysisGPU(bwLungMed, lungWindow, depth);
+        %textureFeatures = textureAnalysisGPU(bwLungMed, lungWindow, depth);
+        textureFeatures = textureAnalysisSerial(bwLungMed, lungWindow, depth);
 
 		imagesCat = cat(3, lungWindow, mediastinal);
         %index = i - first + 1;
--- textureAnalysisSerial.m
+++ textureAnalysisSerial.m
@@ -1,35 +1,28 @@
-function compiledFeatures = textureAnalysisGPU(bwLung, original, depth)
+function compiledFeatures = textureAnalysisSerial(bwLung, original, depth)
 
 	patchSize = 37;
 	patchMid = round(patchSize/2);
     
     lungs = bwLung==0;
     lungs = lungs + (double(bwLung) .* (original));   		% textured lungs on white background
-    %lungGPU = gpuArray(lungs);
-    bwLungGPU = gpuArray(bwLung);
 	%finalMask = zeros(size(lungs,1), size(lungs,2));
     
-    x1 = find(sum(bwLungGPU,1), 1, 'first');
-    x2 = find(sum(bwLungGPU,1), 1, 'last');
-    y1 = find(sum(bwLungGPU,2), 1, 'first');
-    y2 = find(sum(bwLungGPU,2), 1, 'last');
-    bwLungs = gather(bwLungGPU);
-    clear('bwLungs');
+    x1 = find(sum(bwLung,1), 1, 'first');
+    x2 = find(sum(bwLung,1), 1, 'last');
+    y1 = find(sum(bwLung,2), 1, 'first');
+    y2 = find(sum(bwLung,2), 1, 'last');
     
     lungs = lungs(y1-1:y2+1, x1-1:x2+1);					% cropped lungs
-    %lungs = lungsGPU;
+
     jEnd = size(lungs,2)-patchMid+1;
     iEnd = size(lungs,2)-patchMid+1;
     
     %imshow(lungs)
     allPatches = im2col(lungs, [patchSize patchSize], 'sliding');
     allPatches(allPatches==1) = NaN;
-    %allPatchesGPU = gpuArray(allPatches);
 	
 	
 	[r c] = find(isnan(allPatches));
-	%patches = gather(allPatchesGPU);
-    %clear('allPatchesGPU');
     patches = allPatches;
 	patches(:,c) = [];										% remove patches with non-lung white parts
 	%size(patches,2)
@@ -43,6 +36,10 @@ function compiledFeatures = textureAnalysisGPU(bwLung, original, depth)
 	f4 = 'dct';
 	f5 = 'runlength';
 	f6 = 'class';
+    
+    g1 = 'name';
+    g2 = 'values';
+    g3 = 'binEdges';
 	
 	if (patchesEnd>0)
 		compiledFeatures(1, patchesEnd) = struct(f1, zeros(patchSize), f2, NaN, f3, NaN, f4, NaN, f5, NaN);
@@ -50,68 +47,68 @@ function compiledFeatures = textureAnalysisGPU(bwLung, original, depth)
 		%statistics = zeros(1, 5);
 		
 		for i=1:size(patches,2)
-			reshapedPatchGPU = gpuArray(reshape(patches(:,i),patchSize,patchSize));
+			reshapedPatch = reshape(patches(:,i),patchSize,patchSize);
+            textureHistos(1,7) = struct(g1, ' ', g2, NaN, g3, NaN);
             %reshapedPatchGPU = reshape(patches(:,i),patchSize,patchSize);
 			%glcm = graycomatrix(uint8(reshapedPatchGPU*255), 'NumLevels', 16);
 			%stats = graycoprops(glcm, {'Contrast', 'Correlation', 'Energy', 'Homogeneity'});
 			%statistics(:) = [stats.Energy, stats.Contrast, stats.Correlation, stats.Homogeneity, entropy(uint8(reshapedPatch*255))];
-			rpHist = histogram(reshapedPatchGPU,356);
-			rpGausHist = histogram(imgaussfilt(reshapedPatchGPU),256);
-			rpLaplaceHist = histogram(imfilter(reshapedPatchGPU, fspecial('laplacian', 0.8)),256);
-			[rpFirstDerX rpFirstDerY] = gradient(reshapedPatchGPU);
+            textureHistos(1).name = 'rpHist';
+			[textureHistos(1).values textureHistos(1).binEdges] = histcounts(reshapedPatch);
+			rpGausHist = histogram(imgaussfilt(reshapedPatch));
+			rpLaplaceHist = histogram(imfilter(reshapedPatch, fspecial('laplacian', 0.8)));
+			[rpFirstDerX rpFirstDerY] = gradient(reshapedPatch);
 			[rpSecDerX rpDerYX] = gradient(rpFirstDerX);
 			[rpDerXY rpSecDerY] = gradient(rpFirstDerY);
             rpFirstXHist = histogram(rpFirstDerX,256);
             rpFirstYHist = histogram(rpFirstDerY,256);
             rpSecXHist = histogram(rpSecDerX,256);
             rpSecYHist = histogram(rpSecDerY,256);
-            filteredTextureHist = cat(1, rpHist, rpGausHist, rpLaplaceHist, rpFirstXHist, rpFirstYHist, rpSecXHist, rpSecYHist);
+            %filteredTextureHist = cat(1, rpHist, rpGausHist, rpLaplaceHist, rpFirstXHist, rpFirstYHist, rpSecXHist, rpSecYHist);
+            %filteredTextureHist = {rpHist, rpGausHist, rpLaplaceHist, rpFirstXHist, rpFirstYHist, rpSecXHist, rpSecYHist};
             
-            stats = zeros(4,size(filteredTextureHist,1))
+            stats = zeros(4,size(textureHistos,1))
             
-            for k=1:4           %iterating through stats: mean, std2, skew, kurtosis, entropy
-                for k=1:size(filteredTextureHist,1)
-                    switch i
+            for j=1:4           %iterating through stats: mean, std2, skew, kurtosis, entropy
+                for k=1:size(textureHistos,1)
+                    switch j
                         case 1
                             meanBin = zeros(1,256);
                             for m=1:256
-                                meanBin(m) = mean(filteredTextureHist(k).Values(m)*(filteredTextureHist(k).BinWidth*m));
+                                meanBin(m) = mean(textureHistos(k).Values(m)*(textureHistos(k).BinWidth*m));
                             end
-                            stats(i, k) = mean(meanBin);
+                            stats(j, k) = mean(meanBin);
                         case 2
                             stdBin = zeros(1,256);
                             for m=1:256
-                                stdBin(m) = std2(filteredTextureHist(k).Values(m)*(filteredTextureHist(k).BinWidth*m));
+                                stdBin(m) = std2(textureHistos(k).Values(m)*(textureHistos(k).BinWidth*m));
                             end
-                            stats(i, k) = std2(stdBin);
+                            stats(j, k) = std2(stdBin);
                         case 3
                             skewBin = zeros(1,256);
                             for m=1:256
-                                skewBin(m) = (((filteredTextureHist(k).Values(m)*(filteredTextureHist(k).BinWidth*m))-stats(1,k))/stats(2,k))^3;
+                                skewBin(m) = (((textureHistos(k).Values(m)*(textureHistos(k).BinWidth*m))-stats(1,k))/stats(2,k))^3;
                             end
-                            stats(i, k) = sum(skewBin)/patchSize^2;
+                            stats(j, k) = sum(skewBin)/patchSize^2;
                         case 4
                             kurtosisBin = zeros(1,256);
                             for m=1:256
-                                kurtosisBin(m) = (((filteredTextureHist(k).Values(m)*(filteredTextureHist(k).BinWidth*m))-stats(1,k))/stats(2,k))^4 - 3;
+                                kurtosisBin(m) = (((textureHistos(k).Values(m)*(textureHistos(k).BinWidth*m))-stats(1,k))/stats(2,k))^4 - 3;
                             end
-                            stats(i, k) = sum(kurtosisBin)/patchSize^2;
+                            stats(j, k) = sum(kurtosisBin)/patchSize^2;
                         %case 5
                             %stats(i, k) = 'entropy';
                     end
                 end
             end
-			%[U S V] = svd(reshapedPatchGPU);
-            
-            
-            %gather(reshapedPatchGPU);
-            %clear('reshapedPatchGPU');
+
+			%[U S V] = svd(reshapedPatch);
 
 			%[rl(1),rl(2),rl(3),rl(4),rl(5),rl(6),rl(7)] = glrlm(reshapedPatch,16,ones(patchSize));
             %[a b c d e f g] = glrlm(reshapedPatchGPU,16,ones(patchSize));
             %[a b c d e f g] = [ 0 0 0 0 0 0 0 ];
 			%textures{i} = patch;
-			compiledFeatures(i) = struct(f1, reshapedPatchGPU, f2, depth, f3, [0 0 0], f4, NaN, f5, 0);
+			compiledFeatures(i) = struct(f1, reshapedPatch, f2, depth, f3, [0 0 0], f4, NaN, f5, 0);
 			%features(i,:) = cat(2, statistics(1), statistics(2), statistics(3), statistics(4), statistics(5), rl(1), rl(2), rl(3), rl(4), rl(5), rl(6), rl(7));
 			%features(i,:) = cat(2, statistics(1), statistics(2), statistics(3), statistics(4), statistics(5));
 		end
